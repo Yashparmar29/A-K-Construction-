9b417a5d6be04ae79ad33119ecaa7408f3d0bc7e
yash parmar <parmaryas29@gmail.com>
2026-05-20 22:04:49 +0530

    update in contact servlet

--- src/main/java/com/akconstruction/model/Contact.java
+++ src/main/java/com/akconstruction/model/Contact.java
@@ -28,6 +28,5 @@ public class Contact {
     public String getPhone() { return phone; }
     public void setPhone(String phone) { this.phone = phone; }
 
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+    
 }
