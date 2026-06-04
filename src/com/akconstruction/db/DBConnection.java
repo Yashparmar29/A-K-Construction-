28fd7c68f004877354406eb4a07889d7fab2efaa
yash parmar <parmaryas29@gmail.com>
2026-06-04 22:40:22 +0530

    add packages in dbconnection

--- src/com/akconstruction/db/DBConnection.java
+++ src/com/akconstruction/db/DBConnection.java
@@ -1,6 +1,8 @@
 package com.akconstruction.db;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ak_construction";
