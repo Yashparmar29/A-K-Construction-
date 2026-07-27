9d9765a6322f39a0cc022f2967649b8e6c1367f9
yash parmar <parmaryas29@gmail.com>
2026-07-27 23:24:27 +0530

    update run_bat file

--- run.bat
+++ run.bat
@@ -4,6 +4,9 @@ setlocal
 :: Java and Maven paths
 set "JAVA_HOME=C:\Program Files\Java\jdk-25.0.2"
 set "PATH=%JAVA_HOME%\bin;C:\Users\1\.m2\wrapper\dists\apache-maven-3.9.6-bin\3311e1d4\apache-maven-3.9.6\bin;%PATH%"
+set "SPRING_DATASOURCE_URL=jdbc:mysql://localhost:3306/ak_construction?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true"
+set "SPRING_DATASOURCE_USERNAME=root"
+set "SPRING_DATASOURCE_PASSWORD=yash"
 
 echo =============================================
 echo   A K Construction - Spring Boot Launcher
